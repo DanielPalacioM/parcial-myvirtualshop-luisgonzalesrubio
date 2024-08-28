443043356ba93f1481191407480fa4c139395781
Luis gonzalesrubio <lgsantiago07@gmail.com>
2024-08-27 23:14:21 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'shopiestores',
+  webDir: 'www'
+};
+
+export default config;
